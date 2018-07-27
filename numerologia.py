389e64d14860db51c7fba6b9d0b65fa2de0ddf1e
geraldo netto <geraldonetto@gmail.com>
2018-07-27 12:58:07 -0300

    general cleanup replace somar_letra() by char_to_number() replace calcular_numero_magico() by calculate_magick_number() replace pegar_descricao() by get_sephirot() remove formatar_lista()
    Signed-off-by: geraldo netto <geraldonetto@gmail.com>

--- numerologia.py
+++ numerologia.py
@@ -2,55 +2,65 @@
 # -*- coding: utf-8 -*-
 
 import sys
+from string import ascii_uppercase as letters
 
 if len(sys.argv) < 2:
-	print("Numerologia Cabalistica (c) 2018 Geraldo Netto")
-	print("Uso: %s <nome de batismo>" % str(sys.argv[0]))
-	print
-	print("exemplo:")
-	print("netto@besta01 ~/Desktop $ %s maria" % str(sys.argv[0]))
-	print("M 4 (D ou M ou V)")
-	print("A 1 (A ou J ou S)")
-	print("R 9 (I ou R)")
-	print("I 9 (I ou R)")
-	print("A 1 (A ou J ou S)")
-	print("total: 24")
-	print("resultado: 6")
+	print("Kabbalah Numerology (c) 2018 Geraldo Netto"
+				"\nUso:"
+				"\n$ %s ana"
+				"\nA: 1"
+				"\nN: 5"
+				"\nA: 1"
+				"\ntotal: 7 (Venus, Oxum, Touro/Libra)" % str(sys.argv[0]))
 	sys.exit(0)
 
 
-def somar_letras():
-	acumulador = 0
-	nome = ' '.join(sys.argv[1:]).upper()
-	mapa = {
-		1: ['A', 'J', 'S'],
-		2: ['B', 'K', 'T'],
-		3: ['C', 'L', 'U'],
-		4: ['D', 'M', 'V'],
-		5: ['E', 'N', 'W'],
-		6: ['F', 'O', 'X'],
-		7: ['G', 'P', 'Y'],
-		8: ['H', 'Q', 'Z'],
-		9: ['I', 'R'],
-	}
+def char_to_number():
+	names	 = ' '.join(sys.argv[1:]).upper()
 
-	for caractere in range(len(nome)):
-		for idx_numeros in range(len(mapa)):
-			if nome[caractere] in mapa.values()[idx_numeros]:
-				acumulador = acumulador + mapa.keys()[idx_numeros]
-				# TODO reusar no formatar_lista()
-				letras = '(' + ' '.join(mapa.values()[idx_numeros]).replace(' ', ' ou ') + ')'
-				print("%s %d %s" % (nome[caractere], mapa.keys()[idx_numeros], letras))
-				break
+	total = 0
+	for name in names:
+		value = 0
 
-	return acumulador
+		if name in letters:
+			for char in name:
+				char_index = letters.index(char) + 1
+				if char_index % 9 == 0:
+					value += 9
+
+				else:
+					value += char_index % 9
+
+			total += value
+			print("%s: %d" % (name, value))
+
+	return total
 
 
-def formatar_lista(lista):
-	return '(' + ' '.join(lista).replace(' ', ', ') + ')'
+def calculate_magick_number(total):
+	try:
+		total_str = str(total)
+		total_len = len(total_str)
+		if total_len == 0:
+			# a exception ja tem a mensagem
+			raise ValueError("")
+
+		if total_len == 1:
+			return total
+
+		else:
+			acc = 0
+			for idx in range(total_len):
+				acc += int(total_str[idx])
+
+			return calculate_magick_number(acc)
+
+	except ValueError as ex:
+		print("erro ao calcular numero %s" % total)
+		sys.exit(1)
 
 
-def pegar_descricao(numero):
+def get_sephiroth(number):
 	sephiroth = {
 		1: ['N/A', 'Cosme/Damiao', 'N/A'],
 		2: ['Netuno', 'N/A', 'N/A'],
@@ -63,31 +73,8 @@ def pegar_descricao(numero):
 		9: ['Lua', 'Iemanja', 'Cancer'],
 	}
 
-	return sephiroth[numero]
+	return str(sephiroth[number]).replace("[", "(").replace("]", ")").replace("'", "")
 
-
-
-def calcular_numero_magico(soma):
-	numero_final = str(soma)
-	tamanho_numero_final = len(numero_final)
-
-	if tamanho_numero_final < 1:
-		print("erro ao calcular numero")
-		sys.exit(1)
-
-	elif tamanho_numero_final == 1:
-		return soma
-
-	else:
-		temp = 0
-		for idx in range(len(numero_final)):
-			temp = temp + int(numero_final[idx])
-
-		return calcular_numero_magico(temp)
-
-
-soma = somar_letras()
-resultado = calcular_numero_magico(soma)
-print("total: %s" % soma)
-print("resultado: %d %s" % (resultado, formatar_lista(pegar_descricao(resultado))))
+number = calculate_magick_number(char_to_number())
+print("total: %d %s" % (number, get_sephiroth(number)))
 
